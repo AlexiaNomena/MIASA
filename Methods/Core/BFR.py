01f780b521e5a75149bdbbfc24636648ffb47287
AlexiaNomena <missralexia1@gmail.com>
2023-03-07 16:07:27 +0100

    added

---
+++ Methods/Core/BFR.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  7 15:42:06 2023
+
+@author: raharinirina
+"""
+
+"""
+Implementation of the BFR clustering algorithm: more suitable for high-dimensional Euclidean points 
+but making the strong assumption the cluster points are normaly distributed around their centroids
+https://en.wikipedia.org/wiki/BFR_algorithm#:~:text=The%20BFR%20algorithm%2C%20named%20after,normally%20distributed%20about%20a%20centroid.
+"""
